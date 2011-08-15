dae6e07aa31b84f3c4507eca9a27399444afa936
Krunoslav Husak <kruno@binel.hr>
2011-08-16 01:40:57 +0200

    On load, now defines correct sudoku size and subgrid size.

--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -1,5 +1,6 @@
 class SudokuSolver
   attr_reader :size
+  attr_reader :subgrid_size
 
   def initialize()
     @size = 0
@@ -19,17 +20,17 @@ class SudokuSolver
           end
       )
     end
-    # TODO: determine correct size
-    @size = 2
+    @size = @grid.count
+    @subgrid_size = Math.sqrt(@size)
   end
 
   def value_allowed?(value, row, column)
     return false unless @grid[row].select {|cell| value == cell.value}.empty?
     return false unless @grid.transpose[column].select {|cell| value == cell.value}.empty?
-    start_column = column - (column % @size)
-    start_row = row - (row % @size)
-    @grid[start_row..start_row+@size-1].each do |row_to_check|
-      return false unless row_to_check[start_column..start_column+@size-1].select {|cell| value == cell.value}.empty?
+    start_column = column - (column % @subgrid_size)
+    start_row = row - (row % @subgrid_size)
+    @grid[start_row..start_row+@subgrid_size-1].each do |row_to_check|
+      return false unless row_to_check[start_column..start_column+@subgrid_size-1].select {|cell| value == cell.value}.empty?
     end
     true
   end
