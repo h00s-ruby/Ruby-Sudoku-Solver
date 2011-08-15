c1b5c59a07826ae9e242b82e259a4db46d18f473
Krunoslav Husak <kruno@binel.hr>
2011-08-16 01:34:38 +0200

    Implemented method for checking if value is allowed at specified row and column.

--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -3,12 +3,12 @@ class SudokuSolver
 
   def initialize()
     @size = 0
-    @matrix = Array.new
+    @grid = Array.new
   end
 
   def load_from_file(file_path)
     File.open(file_path, 'r').each_line do |line|
-      @matrix.push(
+      @grid.push(
           line.split.collect do |value|
             value = value.to_i
             if value > 0
@@ -19,12 +19,24 @@ class SudokuSolver
           end
       )
     end
-    @size = @matrix.count
+    # TODO: determine correct size
+    @size = 2
+  end
+
+  def value_allowed?(value, row, column)
+    return false unless @grid[row].select {|cell| value == cell.value}.empty?
+    return false unless @grid.transpose[column].select {|cell| value == cell.value}.empty?
+    start_column = column - (column % @size)
+    start_row = row - (row % @size)
+    @grid[start_row..start_row+@size-1].each do |row_to_check|
+      return false unless row_to_check[start_column..start_column+@size-1].select {|cell| value == cell.value}.empty?
+    end
+    true
   end
 
   def to_s
     output = ''
-    @matrix.each do |row|
+    @grid.each do |row|
       output += row.collect {|column| column.to_s}.join(' ') + "\n"
     end
     output
@@ -52,4 +64,4 @@ end
 
 sudoku = SudokuSolver.new
 sudoku.load_from_file('sudoku.txt')
-puts sudoku.to_s
+puts sudoku.value_allowed? 2, 2, 1
