9fa28921ceca40e77a58c2000f266d1b8d90132e
Krunoslav Husak <kruno@binel.hr>
2011-08-15 21:54:12 +0200

    Added initialize method for SudokuSolver class.

--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -1,3 +1,18 @@
 class SudokuSolver
+  attr_reader :size
 
+  def initialize(size = 3)
+    @size = size
+    @matrix = Hash.new
+  end
+
+  def load(file)
+    
+  end
+
+  class Cell
+    def initialize(value)
+
+    end
+  end
 end
