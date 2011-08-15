85061bd5c823e85f9d683774d00c2f1fa63253fd
Krunoslav Husak <kruno@binel.hr>
2011-08-15 23:49:28 +0200

    Integer values in sudoku replaced with Cell objects. Overriden to_s methods in SudokuSolver and Cell.

--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -1,25 +1,55 @@
 class SudokuSolver
-  attr_reader :matrix
+  attr_reader :size
 
   def initialize()
     @size = 0
     @matrix = Array.new
   end
 
-  def load(file_path)
+  def load_from_file(file_path)
     File.open(file_path, 'r').each_line do |line|
-      @matrix.push(line.split)
+      @matrix.push(
+          line.split.collect do |value|
+            value = value.to_i
+            if value > 0
+              Cell.new(value, true)
+            else
+              Cell.new(nil)
+            end
+          end
+      )
     end
+    @size = @matrix.count
+  end
+
+  def to_s
+    output = ''
+    @matrix.each do |row|
+      output += row.collect {|column| column.to_s}.join(' ') + "\n"
+    end
+    output
   end
 
   class Cell
-    def initialize(value)
+    attr_accessor :value
+    attr_reader :predefined
 
+    def initialize(value, predefined = false)
+      @value = value
+      @predefined = predefined
+    end
+
+    def empty?
+      @value.nil? ? true : false
+    end
+
+    def to_s
+      empty? ? '?' : value
     end
   end
 
 end
 
 sudoku = SudokuSolver.new
-sudoku.load('sudoku.txt')
-puts sudoku.matrix.to_s
+sudoku.load_from_file('sudoku.txt')
+puts sudoku.to_s
